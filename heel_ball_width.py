5b1c14f7fd18ed072ee063cf2ee52e683e3b0e1a
jhhg1997 <jh16644@bristol.ac.uk>
2019-10-28 13:57:13 +0000

    Scatter plot of heel to ball width

--- heel_ball_width.py
+++ heel_ball_width.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import skew
 
 
 points = pd.read_csv('MDM Footscan Data\C. Measures\Default Measures.csv', header=0)
@@ -10,5 +11,18 @@ heel= points.iloc[:,9].values
 ratio= (ball/heel)
 print(ratio)
 
-plt.hist(ratio)
+#plt.hist(ratio, bins=np.arange(min(ratio), max(ratio) +0.01, 0.01))
+#plt.show
+
+mean = np.mean(ratio)
+print("mean =",(mean))
+
+skew = skew(ratio)
+print("skew =",(skew))
+
+print("minimum =", min(ratio))
+print("maximun =",max(ratio))
+
+plt.scatter(heel, ball)
 plt.show
+
