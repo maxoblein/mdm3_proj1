d747fab9c242f78e7d18cb43284a4c95d43ab53b
maxoblein <max.oblein@gmail.com>
2019-10-31 14:40:32 +0000

    working on finding big toe currently not working

--- vrml_reader.py
+++ vrml_reader.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import re
+from scipy.spatial import ConvexHull
 import sys
 
 def find_coords(filename):
@@ -51,20 +51,46 @@ def leftright(scan_array):
         Side = 'R'
     return Side
 
+def slices(scan_array):
+    front_array = np.ones(3)
+    slice_range = np.linspace(scan_array[0,2],scan_array[-1,2],200)
+    for i in range(1,np.size(slice_range)):
+        slice_array = np.ones(3)
+        for j in range(np.shape(scan_array)[0]):
+            if scan_array[j,2] > slice_range[i-1] and scan_array[j,2] < slice_range[i] :
+                slice_array = np.vstack((slice_array,scan_array[j]))
+        slice_array = np.delete(slice_array,0,0)
+        front_point =slice_array[np.argmax(slice_array[:,0])]
+        front_array = np.vstack((front_array,front_point))
+    front_array = np.delete(front_array,0,0)
+    return front_array
+
 def Big_Toe_Isol8er(scan_array):
+    tol = 1e-6
     side = leftright(scan_array)
+
     toe_index = np.argwhere(scan_array[:,0] > 0.15)
     toe_array = np.ones(3)
     for i in toe_index:
         toe_array = np.vstack((toe_array,scan_array[i]))
     toe_array = np.delete(toe_array,0,0)
-    Visualise(toe_array)
+
+
     if side == 'L':
         #right side has big toe
+        ind = np.lexsort((toe_array[:,0],toe_array[:,1],toe_array[:,2]))
+        ind = np.flip(ind)
+        sorted_array = toe_array[ind]
+        print(slices(sorted_array))
+
+
         return 0
 
     if side == 'R':
         #left side has big toe
+        ind = np.lexsort((toe_array[:,0],toe_array[:,1],toe_array[:,2]))
+        sorted_array = toe_array[ind]
+
         return 1
 
 
