5c1bd6ca35aa86777126a68ef76a364962130387
unknown <annabellesbale@gmail.com>
2019-11-12 21:50:13 +0000

    predicts based upon ball vs ratio

--- heel_ball_width.py
+++ heel_ball_width.py
@@ -11,6 +11,7 @@ def independence(x,y):
     plt.figure()
     plt.scatter(x,y)
     Z=(np.sqrt(len(x)-3)/2)*np.log((1+r)/(1-r))
+    print("Z =",Z)
     if np.abs(Z)<=1.96:
         print("Data is independent to 95% statistical significance")
         return bool(1)
@@ -25,6 +26,7 @@ def linreg(x,y):
     c=model.intercept_
     m=model.coef_
     print("y =",m[0],"x +",c)
+    return [m[0],c]
     
 def concave(heel,inst):
     out=[]
@@ -35,15 +37,57 @@ def concave(heel,inst):
             out.append(1)
     return(out)
 
+def formatting(ax):
+    ax.set_xlabel('Ball width (mm)',fontsize = 18 )
+    ax.set_ylabel('Heel width (mm)',fontsize = 18 )
+    ax.tick_params(labelsize=14)
+    ax.set_title('Relationship between Ball and Heel Width',fontsize = 20)
+    return ax
+
 points = pd.read_csv('MDM Footscan Data\C. Measures\Default Measures.csv', header=0)
 ball= points.iloc[:,2].values
 instep= points.iloc[:,6].values
 heel= points.iloc[:,9].values
-ratio = heel/ball
+ratioh = heel/ball
+ratiob = ball/heel
 
-independence(heel,ball)
+##indepedence testing
+#independence(heel,ball)
+#
+#independence(heel,ratioh)
 
-independence(heel,ratio)
-linreg(heel,ratio)
+##first line fit
+#coef=linreg(heel,ratioh)
+##
+#x=list(range(47,83))
+#y=[]
+#for item in x:
+#    y.append(float(item)/(coef[0]*float(item)+coef[1]))
+##print(y)
+#
+#plt.figure()
+#ax=plt.subplot(1,1,1)
+#ax.scatter(heel,ball,s=10)
+#ax.plot(x,y,"r-")
+#formatting(ax)
+#plt.show()
+##print(concave(heel,instep))
 
-#print(concave(heel,instep))
+#second line fit
+coef=linreg(ball,ratiob)
+#
+x=list(range(80,105))
+y=[]
+for item in x:
+    y.append(float(item)/(coef[0]*float(item)+coef[1]))
+#print(y)
+    
+
+
+plt.figure()
+ax=plt.subplot(1,1,1)
+ax.scatter(ball,heel,s=10)
+ax.plot(x,y,"r-")
+formatting(ax)
+plt.show()
+##print(concave(heel,instep))
