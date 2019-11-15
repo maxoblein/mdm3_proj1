259c09a4eeae39be2b842a307239f225020ed30a
jhhg1997 <jh16644@bristol.ac.uk>
2019-11-15 11:06:48 +0000

    nn

--- Point cloud heatmap.py
+++ Point cloud heatmap.py
@@ -112,25 +112,28 @@ def Initialise_3d_Swap(inp, size, cut1, cut2, cut3, number, axes):
 
 def nn(inp, vec):
 
-    #X = Initialise_3d(inp, [20, 40], [0.2, 0.25], [0, 0.012], [-0.05, 0.05], [0, 1150], [0,2])
-    #x = Initialise_3d(inp, [20, 40], [0.2, 0.25], [0, 0.012], [-0.05, 0.05], [1151, 1289], [0,2])
-    #XS = Initialise_3d_Swap(inp, [20, 40], [0.15, 0.2], [0, 0.02], [-0.05, 0], [0, 1150], [0,2])
-    #xS = Initialise_3d_Swap(inp, [20, 40], [0.15, 0.2], [0, 0.02], [-0.05, 0], [1151, 1289], [0,2])
-    #X = np.dstack((X,XS))
-    #x = np.dstack((x,xS))
-    plt.imshow(XS[:,:,573], origin = 'upper')
+    X = Initialise_3d(inp, [40, 40], [0.2, 0.25], [0, 0.012], [-0.05, 0.05], [0, 300], [0,2])
+    x = Initialise_3d(inp, [40, 40], [0.2, 0.25], [0, 0.012], [-0.05, 0.05], [501, 600], [0,2])
+    #XS = Initialise_3d_Swap(inp, [40, 40], [0.15, 0.2], [0, 0.02], [-0.05, 0], [0, 1000], [0,2])
+    #xS = Initialise_3d_Swap(inp, [40, 40], [0.15, 0.2], [0, 0.02], [-0.05, 0], [1001, 1289], [0,2])
+    #XSA = Initialise_3d_Swap(inp, [40, 40], [0.1, 0.15], [0, 0.03], [-0.05, -0.01], [0, 1000], [1,0])
+    #xSA = Initialise_3d_Swap(inp, [40, 40], [0.075, 0.16], [0, 0.03], [-0.05, -0.01], [1001, 1289], [1,0])
     
-    X = X.reshape(1150, 21, 41, 1)
-    x = x.reshape(138, 21, 41, 1)
+    #X = np.dstack((X,np.dstack((XS,XSA))))
+    #x = np.dstack((x,np.dstack((xS,xSA))))
+    #plt.imshow(X[:,:,2000], origin = 'upper')
     
-    Y = to_categorical(vec[0:1150])
-    y = to_categorical(vec[1151:1289])
+    X = X.reshape(300, 41, 41, 1)
+    x = x.reshape(99, 41, 41, 1)
+    
+    Y = to_categorical(vec[0:300])
+    y = to_categorical(vec[501:600])
     
     loaded_model = Sequential()
     
     #loaded_model = load_model('model.h5')
     
-    loaded_model.add(Conv2D(64, kernel_size = 3, activation = 'relu', input_shape = (21,41,1)))
+    loaded_model.add(Conv2D(64, kernel_size = 3, activation = 'relu', input_shape = (41,41,1)))
     loaded_model.add(Conv2D(32, kernel_size = 3, activation = 'relu'))
     loaded_model.add(Flatten())
     loaded_model.add(Dense(2, activation = 'softmax'))
@@ -160,7 +163,7 @@ def nn(inp, vec):
     print(y[:10])
     
     
-inp = os.listdir('C:/Users/joere/OneDrive/Desktop/MDM Footscan Data/VRML')
+inp = os.listdir()
 inp = inp[:-4]
 
 LR = load_points_from_file('LR.csv')
