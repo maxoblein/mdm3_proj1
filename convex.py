ba142fc80656962e036e3cf4e58617c182931af6
unknown <annabellesbale@gmail.com>
2019-11-25 23:10:52 +0000

    classifier finished

--- convex.py
+++ convex.py
@@ -1,11 +1,10 @@
 from vrml_reader import find_coords
 from vrml_reader import leftright
-from vrml_reader import Visualise
+#from vrml_reader import Visualise
 import matplotlib.pyplot as plt
 import numpy as np
 
-def convex(filename):
-    data = find_coords(filename)
+def convex(data):
     
     #to chop off the ankle
     foot = []
@@ -38,7 +37,7 @@ def convex(filename):
     #empty array
     for i in range (len(foot)):
        a = foot[i, 0]
-       if 0.0795 < a < 0.0805 :
+       if 0.1395 < a < 0.1405 :
            instep.append(foot[i])
     #For all data find where y values are less than ..., add those data points to the footprint array       
     instep = np.vstack((instep))
@@ -46,49 +45,28 @@ def convex(filename):
     #plot to check
     #plt.scatter(instep[:,0], instep[:,2])
     
-    #to separate ball
-    ball = []
-    #empty array 
-    for i in range (len(foot)):
-       a = foot[i, 0]
-       if 0.1495 < a < 0.1505  :
-           ball.append(foot[i])
-    #For all data find where y values are less than ..., add those data points to the footprint array       
-    ball = np.vstack((ball))
-    #combine all arrays within foot print
-    #plot to check
-    #plt.scatter(ball[:,0], ball[:,2])
-    
     side=leftright(data)
     if side == "L":
         #find edge point of heel
         heel_edge=heel[np.argmin(heel[:,2])]
-        plt.scatter(heel_edge[0], heel_edge[2])
+        #plt.scatter(heel_edge[0], heel_edge[2])
         #find edge point of instep
         instep_edge=instep[np.argmin(instep[:,2])]
-        plt.scatter(instep_edge[0], instep_edge[2])
-        #find edge point of ball
-        ball_edge=ball[np.argmin(ball[:,2])]
-        plt.scatter(ball_edge[0], ball_edge[2])
+        #plt.scatter(instep_edge[0], instep_edge[2])
     else:
         #find edge point of heel
         heel_edge=heel[np.argmax(heel[:,2])]
-        plt.scatter(heel_edge[0], heel_edge[2])
+        #plt.scatter(heel_edge[0], heel_edge[2])
         #find edge point of instep
         instep_edge=instep[np.argmax(instep[:,2])]
-        plt.scatter(instep_edge[0], instep_edge[2])
-        #find edge point of ball
-        ball_edge=ball[np.argmax(ball[:,2])]
-        plt.scatter(ball_edge[0], ball_edge[2])
-    
-    #find gradients between heel, instep and ball edge point
-    grad1=abs((heel_edge[2]-instep_edge[2])/(heel_edge[0]-instep_edge[0]))
-    grad2=abs((ball_edge[2]-instep_edge[2])/(ball_edge[0]-instep_edge[0]))
-    if grad1<grad2:
-        convexity=0
-    else:
-        convexity=1
-    print(convexity)
-    return convexity
+        #plt.scatter(instep_edge[0], instep_edge[2])
         
-convex("MDM Footscan Data/VRML/000454.wrl")
+    #check whether heel or instep were further out
+    if abs(heel_edge[2])<abs(instep_edge[2]):
+        convexity=str(1)
+    else:
+        convexity=str(0)
+    return convexity
+      
+data = find_coords("MDM Footscan Data/VRML/000454.wrl")  
+convex(data)
